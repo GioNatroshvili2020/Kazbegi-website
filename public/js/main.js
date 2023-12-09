44b84911b6ba3ab0811143f7961d58fbfb99c3c2
Giorgi Chankseliani <giorgi.chanqseliani99@gmail.com>
2023-12-09 17:47:21 +0100

    style fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,27 @@
 function onLoad(header) {
-    const headerPlaceholder = document.getElementById('header-placeholder');
-    headerPlaceholder.innerHTML = header;
+  const headerPlaceholder = document.getElementById("header-placeholder");
+  headerPlaceholder.innerHTML = header;
 
-    const pages = Array.from(document.querySelectorAll('.navbar a'));
-    const path = location.pathname;
-    const currentPage = pages.find(p => path.includes(p.pathname))
-    if (currentPage) currentPage.classList.add('active');
+  const pages = Array.from(document.querySelectorAll(".navbar a"));
+  const path = location.pathname;
+  const currentPage = pages.find((p) => path.includes(p.pathname));
+  if (currentPage) currentPage.classList.add("active");
 
-    const ham = document.querySelector('.ham-menu')
-    const nav = document.querySelector('.navbar')
-    ham.addEventListener("click", ()=>{
-        ham.classList.toggle('active')
-        nav.classList.toggle('active')
-    })
+  const ham = document.querySelector(".ham-menu");
+  const nav = document.querySelector(".navbar");
+  ham.addEventListener("click", () => {
+    ham.classList.toggle("active");
+    nav.classList.toggle("active");
+  });
 
-    const loadder = document.getElementById('preloadder')
-    loadder.style.display = "none";
+  const loadder = document.getElementById("preloadder");
+  loadder.style.display = "none";
 }
 
-window.addEventListener("load",()=>{
-    fetch('header.html').then(response => response.text()).then((header) => {
-        onLoad(header);
+window.addEventListener("load", () => {
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((header) => {
+      onLoad(header);
     });
 });
-
