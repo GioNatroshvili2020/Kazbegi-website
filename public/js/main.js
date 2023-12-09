b9ffaf4d007f9d1a51938310b67f775933fd8a37
Giorgi Chankseliani <giorgi.chanqseliani99@gmail.com>
2023-12-09 18:00:38 +0100

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,4 @@
 function onLoad(header) {
-  const headerPlaceholder = document.getElementById("header-placeholder");
-  headerPlaceholder.innerHTML = header;
-
-  const pages = Array.from(document.querySelectorAll(".navbar a"));
-  const path = location.pathname;
-  const currentPage = pages.find((p) => path.includes(p.pathname));
-  if (currentPage) currentPage.classList.add("active");
-
   const ham = document.querySelector(".ham-menu");
   const nav = document.querySelector(".navbar");
   ham.addEventListener("click", () => {
@@ -19,9 +11,5 @@ function onLoad(header) {
 }
 
 window.addEventListener("load", () => {
-  fetch("header.html")
-    .then((response) => response.text())
-    .then((header) => {
-      onLoad(header);
-    });
+  onLoad(header);
 });
